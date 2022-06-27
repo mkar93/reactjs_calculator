4a4aff7f6fe1fd2a0be05f9b7504e01abe7ba5c6
mkar93 <52184177+mkar93@users.noreply.github.com>
2022-06-27 13:24:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,39 @@ function App() {
   const [calc, setCalc] = useState("");            
   const [result, setResult] = useState("");
   const ops = ["/", "*", "+", "-", "%"];
+  const opsOne = ["/", "*", "+", "-", "%", "."];
   const ops_dot = ["."];
   const ops_all = ops + ops_dot;
+  
 
   const updateCalc = value => {
 
-    
     //leading zero mega fix
     if(calc.charAt(0) === "0" && calc == 0) {      
       setCalc(calc.replace(0, value));
       return;
     }
 
+    if(opsOne.includes(calc.slice(-3, -1))) {
+      console.log("T E S T")
+      
+    }
+
+
     //2nd leading(after operator) zero fix wtf
-    //if(ops.some( ai => calc.includes(ai.slice(-3))) && calc.charAt(calc.length-2) == 0) {
-    //  if(calc.charAt(calc.length-1) == "." || (value >=0 && value <=9)) {
-    //    setCalc(calc + value)
-    //  }
-    //  else {
-    //    return;
-    //  }
-    //}
+    // if(ops.some( ai => calc.includes(ai.slice(-3))) && calc.charAt(calc.length-2) == 0) {
+    //   if(calc.charAt(calc.length-1) == ".") {
+    //     if(value >=0 && value <=9) {
+    //       setCalc(calc + value)
+    //     }
+    //     else {
+    //       return;
+    //     }      
+    //   }
+    //   else {
+    //     return;
+    //   }
+    // }
 
     //doesn't allow two operations or dot in a row or at start
     if((ops_all.includes(value) && calc === "") || (ops_all.includes(value) && ops_all.includes(calc.slice(-1)))) {
@@ -47,10 +59,14 @@ function App() {
 
     setCalc(calc + value);
 
+
+
     //za testiranje u konzoli
+    console.log("==========================")
     console.log("value ", value)
     console.log("calc ", calc)
     console.log("result ", result)
+    console.log("==========================")
 
   }
 
@@ -70,6 +86,7 @@ function App() {
     return digits;
   }
 
+
   //equals function
   const calculate = () => {
     if(calc.charAt(0) === "") {
@@ -88,20 +105,21 @@ function App() {
 
   //reciprocal function
   const reciprocal = () => {
-    if(calc.length === 0 || calc == 0) {
-      setResult(result + "Ne možete dijeliti s nulom\n") //ne mozes dijelit na pocetku ili ako je nula
+    if(calc.length === 0 || calc == 0) {                                              //checks if equation is empty or == 0
+      setResult(result + "Ne možete dijeliti s nulom\n") 
       return;
     }
+
     setCalc(eval(1/calc).toString());
     setResult(result + "1/" + calc + " = " + 1/calc + "\n");
   }
 
   //to the power of
   const pwr = () => {
-    if(calc.length === 0 ) {
-      return;           //ne mozes kvadrirat ako nema niceg i ako izraz nije završen
-    }                   //|| ops.some( ai => calc.includes(ai.slice(-1)))
-
+    if(calc.length === 0 || opsOne.some( ai => calc.includes(ai.slice(-1))) || calc == 0) {        //checks if operator is last element or equation is empty
+      return;           
+    }                 
+   
     setCalc(eval(calc*calc).toString());
     setResult(result + calc + '\u00B2' + " = " + calc*calc + "\n");
   }
@@ -148,15 +166,12 @@ function App() {
   }; 
 
   
-
   return (
     <div className="App">
       <div className="calculator">
         <div className="equationDisplay">
           { result ? result : ""}   
         </div>
-
-        
 
         <div className="display">        
           { calc || "0" }         
@@ -201,7 +216,6 @@ function App() {
   );
   
 }
-
 
 
 export default App;
